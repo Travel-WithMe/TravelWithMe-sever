16d35a29421242ab0d48bd9973015dfb4e907748
jambin <gksmfcksqls@gmail.com>
2023-04-02 06:01:11 +0900

    feat - #2: Member Entityt 구현

--- travel-with-me/src/main/java/com/frog/travelwithme/TravelWithMeApplication.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/TravelWithMeApplication.java
@@ -2,7 +2,9 @@ package com.frog.travelwithme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TravelWithMeApplication {
 
