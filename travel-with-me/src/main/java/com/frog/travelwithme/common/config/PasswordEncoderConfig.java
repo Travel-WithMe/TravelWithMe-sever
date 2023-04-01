25ccad3fdb6ab2dfede897a711fcf93afb6d8d4a
jambin <gksmfcksqls@gmail.com>
2023-04-02 06:08:10 +0900

    feat - #2: PasswordEncoder Bean 생성

---
+++ travel-with-me/src/main/java/com/frog/travelwithme/common/config/PasswordEncoderConfig.java
@@ -0,0 +1,20 @@
+package com.frog.travelwithme.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * PasswordEncoderConfig 설명: PasswordEncoder Bean 생성
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/03/31
+ **/
+@Configuration
+public class PasswordEncoderConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
