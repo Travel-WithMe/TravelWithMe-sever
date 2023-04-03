2db43166af7f07ca546de8a201e6dd0ae43aa6a9
jambin <gksmfcksqls@gmail.com>
2023-04-03 22:05:34 +0900

    security - #2: security 구현

---
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/security/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,80 @@
+package com.frog.travelwithme.global.security.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.frog.travelwithme.common.config.AES128Config;
+import com.frog.travelwithme.domain.member.entity.Member;
+import com.frog.travelwithme.domain.member.service.MemberService;
+import com.frog.travelwithme.global.redis.RedisDao;
+import com.frog.travelwithme.global.security.auth.dto.LoginDto;
+import com.frog.travelwithme.global.security.auth.dto.TokenDto;
+import com.frog.travelwithme.global.security.auth.jwt.JwtTokenProvider;
+import com.frog.travelwithme.global.security.auth.userdetails.CustomUserDetails;
+import com.frog.travelwithme.global.security.auth.utils.Responder;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Duration;
+
+/**
+ * JwtAuthenticationFilter 설명: 로그인 검증 및 JWT 발급
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/03/28
+ **/
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AES128Config aes128Config;
+    private final MemberService memberService;
+    private final RedisDao redisDao;
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // ServletInputStream을 LoginDto 객체로 역직렬화
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        CustomUserDetails customUserDetails = (CustomUserDetails) authResult.getPrincipal();
+        TokenDto tokenDto = jwtTokenProvider.generateTokenDto(customUserDetails);
+        String accessToken = tokenDto.getAccessToken();
+        String refreshToken = tokenDto.getRefreshToken();
+        String encryptedRefreshToken = aes128Config.encryptAes(refreshToken);
+        jwtTokenProvider.accessTokenSetHeader(accessToken, response);
+        jwtTokenProvider.refresshTokenSetHeader(encryptedRefreshToken, response);
+        Member findMember = memberService.findVerifiedMember(customUserDetails.getId());
+        Responder.loginSuccessResponse(response, findMember);
+
+        // 로그인 성공시 Refresh Token Redis 저장 ( key = Email / value = Refresh Token )
+        int refreshTokenExpirationMinutes = jwtTokenProvider.getRefreshTokenExpirationMinutes();
+        redisDao.setValues(findMember.getEmail(), refreshToken, Duration.ofMinutes(refreshTokenExpirationMinutes));
+
+        this.getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
+    }
+}
---
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/security/auth/filter/JwtVerificationFilter.java
@@ -0,0 +1,84 @@
+package com.frog.travelwithme.global.security.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.frog.travelwithme.global.exception.BusinessLogicException;
+import com.frog.travelwithme.global.exception.ErrorResponse;
+import com.frog.travelwithme.global.redis.RedisDao;
+import com.frog.travelwithme.global.security.auth.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * JwtVerificationFilter 설명: JWT 검증
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/03/29
+ **/
+@Slf4j
+@RequiredArgsConstructor
+public class JwtVerificationFilter extends OncePerRequestFilter {
+    // 인증에서 제외할 url
+    private static final List<String> EXCLUDE_URL =
+            List.of("/",
+                    "/h2",
+                    "/members/signup",
+                    "/members/login",
+                    "/members/reissue");
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisDao redisDao;
+
+    // JWT 인증 정보를 현재 쓰레드의 SecurityContext에 저장(가입/로그인/재발급 Request 제외)
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String accessToken = jwtTokenProvider.resolveAccessToken(request);
+            if (StringUtils.hasText(accessToken) && doNotLogout(accessToken)
+                    && jwtTokenProvider.validateToken(accessToken, response)) {
+                setAuthenticationToContext(accessToken);
+            }
+        // TODO: 예외처리 리팩토링
+        } catch (RuntimeException e) {
+            if (e instanceof BusinessLogicException) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                String json = objectMapper.writeValueAsString(ErrorResponse.of(((BusinessLogicException) e).getExceptionCode()));
+                response.getWriter().write(json);
+                response.setStatus(((BusinessLogicException) e).getExceptionCode().getStatus());
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean doNotLogout(String accessToken) {
+        String isLogout = redisDao.getValues(accessToken);
+        return ObjectUtils.isEmpty(isLogout);
+    }
+
+    // EXCLUDE_URL과 동일한 요청이 들어왔을 경우, 현재 필터를 진행하지 않고 다음 필터 진행
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        boolean result = EXCLUDE_URL.stream().anyMatch(exclude -> exclude.equalsIgnoreCase(request.getServletPath()));
+        log.info("# Exclude url check = {}, result check = {}", request.getServletPath(), result);
+
+        return result;
+    }
+
+    private void setAuthenticationToContext(String accessToken) {
+        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        log.info("# Token verification success!");
+    }
+}
