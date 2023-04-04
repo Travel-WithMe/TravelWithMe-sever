fe72b9b1388b9d1dc024dc933e18b26651ae1483
jambin <gksmfcksqls@gmail.com>
2023-04-04 22:46:05 +0900

    comment: 부가설명 주석 추가

--- travel-with-me/src/main/java/com/frog/travelwithme/global/dto/PageInfo.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/dto/PageInfo.java
@@ -3,6 +3,12 @@ package com.frog.travelwithme.global.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * PageInfo 설명: 페이지 정보 관리
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/04/02
+ **/
 @AllArgsConstructor
 @Getter
 public class PageInfo {
--- travel-with-me/src/main/java/com/frog/travelwithme/global/dto/SingleResponseDto.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/dto/SingleResponseDto.java
@@ -3,6 +3,12 @@ package com.frog.travelwithme.global.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * SingleResponseDto 설명: 단일 공통 응답 Dto
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/04/02
+ **/
 @AllArgsConstructor
 @Getter
 public class SingleResponseDto<T> {
