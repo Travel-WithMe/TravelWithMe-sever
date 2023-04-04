b7f35916110fa32ce7a6908d3d50196c86f1bb35
jambin <gksmfcksqls@gmail.com>
2023-04-04 22:46:05 +0900

    comment: 부가설명 주석 추가

--- travel-with-me/src/main/java/com/frog/travelwithme/global/dto/MultiResponseDto.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/dto/MultiResponseDto.java
@@ -5,6 +5,12 @@ import org.springframework.data.domain.Page;
 
 import java.util.List;
 
+/**
+ * MultiResponseDto 설명: 다중 공통 응답 Dto
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/04/02
+ **/
 @Getter
 public class MultiResponseDto<T> {
     private List<T> data;
--- travel-with-me/src/main/java/com/frog/travelwithme/global/dto/PagelessMultiResponseDto.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/dto/PagelessMultiResponseDto.java
@@ -5,6 +5,12 @@ import lombok.Getter;
 
 import java.util.List;
 
+/**
+ * PagelessMultiResponseDto 설명: 페이지 정보를 제외한 다중 응답 Dto
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/04/02
+ **/
 @Getter
 @AllArgsConstructor
 public class PagelessMultiResponseDto<T> {
