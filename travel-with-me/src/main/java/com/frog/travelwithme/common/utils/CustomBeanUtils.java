538eeb8ca0c114ff83aaca45ae508e89d940189b
jambin <gksmfcksqls@gmail.com>
2023-04-05 17:08:32 +0900

    feat: CustomBeanUtils 구현

---
+++ travel-with-me/src/main/java/com/frog/travelwithme/common/utils/CustomBeanUtils.java
@@ -0,0 +1,34 @@
+package com.frog.travelwithme.common.utils;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+
+/**
+ * CustomBeanUtils 설명: 동일한 Class간 null이 아닌 값만 복사
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/04/03
+ **/
+@Component
+public class CustomBeanUtils<T> {
+    public T copyNonNullProperties(T source, T destination) {
+        if (source == null || destination == null || source.getClass() != destination.getClass()) {
+            return null;
+        }
+
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        final BeanWrapper dest = new BeanWrapperImpl(destination);
+
+        for (final Field property : source.getClass().getDeclaredFields()) {
+            Object sourceProperty = src.getPropertyValue(property.getName());
+            if (sourceProperty != null && !(sourceProperty instanceof Collection<?>)) {
+                dest.setPropertyValue(property.getName(), sourceProperty);
+            }
+        }
+        return destination;
+    }
+}
