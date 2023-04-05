77665e0cddb244cf88af24d1a8d304cbe8971e2b
jambin <gksmfcksqls@gmail.com>
2023-04-05 17:07:59 +0900

    rename: Auth 관련 로직 Auth Controller, Service로 이동

--- travel-with-me/src/main/java/com/frog/travelwithme/domain/member/service/MemberService.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/domain/member/service/MemberService.java
@@ -1,6 +1,5 @@
 package com.frog.travelwithme.domain.member.service;
 
-import com.frog.travelwithme.common.config.AES128Config;
 import com.frog.travelwithme.common.utils.CustomBeanUtils;
 import com.frog.travelwithme.domain.member.controller.dto.MemberDto;
 import com.frog.travelwithme.domain.member.entity.Member;
@@ -8,19 +7,11 @@ import com.frog.travelwithme.domain.member.mapper.MemberMapper;
 import com.frog.travelwithme.domain.member.repository.MemberRepository;
 import com.frog.travelwithme.global.exception.BusinessLogicException;
 import com.frog.travelwithme.global.exception.ExceptionCode;
-import com.frog.travelwithme.global.redis.RedisDao;
-import com.frog.travelwithme.global.security.auth.dto.TokenDto;
-import com.frog.travelwithme.global.security.auth.jwt.JwtTokenProvider;
-import com.frog.travelwithme.global.security.auth.userdetails.CustomUserDetails;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.time.Duration;
 import java.util.Optional;
 
 import static com.frog.travelwithme.domain.member.entity.Member.OAuthStatus.NORMAL;
@@ -28,7 +19,7 @@ import static com.frog.travelwithme.global.security.auth.utils.CustomAuthorityUt
 
 /**
  * 작성자: 김찬빈
- * 버전 정보: 1.0.0
+ * 버전 정보: 1.0.1
  * 작성일자: 2023/03/29
  **/
 @Service
@@ -37,9 +28,6 @@ import static com.frog.travelwithme.global.security.auth.utils.CustomAuthorityUt
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final AES128Config aes128Config;
-    private final RedisDao redisDao;
     private final CustomBeanUtils<Member> beanUtils;
     private final MemberMapper mapper;
 
@@ -62,49 +50,12 @@ public class MemberService {
         return mapper.toDto(findMember);
     }
 
-    public MemberDto.Response updateMember(MemberDto.Patch patchDto) {
+    public MemberDto.Response updateMember(MemberDto.Patch patchDto, String email) {
         Member member = mapper.toEntity(patchDto);
-        Member findMember = findVerifiedMember(member.getId());
+        Member findMember = findVerifiedMember(email);
         Member updateMember = beanUtils.copyNonNullProperties(member, findMember);
 
         return mapper.toDto(updateMember);
-    }
-
-    public void reissueAccessToken(HttpServletRequest request, HttpServletResponse response) {
-        String encryptedRefreshToken = jwtTokenProvider.resolveRefreshToken(request);
-        verifiedRefreshToken(encryptedRefreshToken);
-        String refreshToken = aes128Config.decryptAes(encryptedRefreshToken);
-        Claims claims = jwtTokenProvider.parseClaims(refreshToken);
-        String email = claims.getSubject();
-        String redisRefreshToken = redisDao.getValues(email);
-
-        if (redisDao.validateValues(redisRefreshToken) && refreshToken.equals(redisRefreshToken)) {
-            Member findMember = findVerifiedMember(email);
-            CustomUserDetails userDetails = CustomUserDetails.of(findMember);
-            TokenDto tokenDto = jwtTokenProvider.generateTokenDto(userDetails);
-            String newAccessToken = tokenDto.getAccessToken();
-            int refreshTokenExpirationMinutes = jwtTokenProvider.getRefreshTokenExpirationMinutes();
-            redisDao.setValues(refreshToken, newAccessToken,
-                    Duration.ofMinutes(refreshTokenExpirationMinutes));
-            jwtTokenProvider.accessTokenSetHeader(newAccessToken, response);
-        } else throw new BusinessLogicException(ExceptionCode.TOKEN_IS_NOT_SAME);
-    }
-
-    public void logout(HttpServletRequest request) {
-        String encryptedRefreshToken = jwtTokenProvider.resolveRefreshToken(request);
-        verifiedRefreshToken(encryptedRefreshToken);
-        String refreshToken = aes128Config.decryptAes(encryptedRefreshToken);
-        Claims claims = jwtTokenProvider.parseClaims(refreshToken);
-        String email = claims.getSubject();
-        String redisRefreshToken = redisDao.getValues(email);
-        if (!redisDao.validateValues(redisRefreshToken)) {
-            redisDao.deleteValues(email);
-
-            // 로그아웃 시 Access Token Redis 저장 ( key = Access Token / value = "logout" )
-            String accessToken = jwtTokenProvider.resolveAccessToken(request);
-            int accessTokenExpirationMinutes = jwtTokenProvider.getAccessTokenExpirationMinutes();
-            redisDao.setValues(accessToken, "logout", Duration.ofMinutes(accessTokenExpirationMinutes));
-        }
     }
 
     @Transactional(readOnly = true)
@@ -117,12 +68,6 @@ public class MemberService {
     public Member findVerifiedMember(String email) {
         return memberRepository.findByEmail(email)
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
-    }
-
-    private void verifiedRefreshToken(String encryptedRefreshToken) {
-        if (encryptedRefreshToken == null) {
-            throw new BusinessLogicException(ExceptionCode.HEADER_REFRESH_TOKEN_NOT_EXISTS);
-        }
     }
 
     private void verifyExistsEmail(String email) {
---
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/security/auth/service/AuthService.java
@@ -0,0 +1,78 @@
+package com.frog.travelwithme.global.security.auth.service;
+
+import com.frog.travelwithme.common.config.AES128Config;
+import com.frog.travelwithme.domain.member.entity.Member;
+import com.frog.travelwithme.domain.member.repository.MemberRepository;
+import com.frog.travelwithme.global.exception.BusinessLogicException;
+import com.frog.travelwithme.global.exception.ExceptionCode;
+import com.frog.travelwithme.global.redis.RedisService;
+import com.frog.travelwithme.global.security.auth.dto.TokenDto;
+import com.frog.travelwithme.global.security.auth.jwt.JwtTokenProvider;
+import com.frog.travelwithme.global.security.auth.userdetails.CustomUserDetails;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
+
+/**
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/04/05
+ **/
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthService {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AES128Config aes128Config;
+    private final RedisService redisService;
+    private final MemberRepository memberRepository;
+
+    public String reissueAccessToken(String encryptedRefreshToken, HttpServletResponse response) {
+        verifiedRefreshToken(encryptedRefreshToken);
+        String refreshToken = aes128Config.decryptAes(encryptedRefreshToken);
+        Claims claims = jwtTokenProvider.parseClaims(refreshToken);
+        String email = claims.getSubject();
+        String redisRefreshToken = redisService.getValues(email);
+
+        if (redisService.validateValues(redisRefreshToken) && refreshToken.equals(redisRefreshToken)) {
+            Member findMember = findVerifiedMember(email);
+            CustomUserDetails userDetails = CustomUserDetails.of(findMember);
+            TokenDto tokenDto = jwtTokenProvider.generateTokenDto(userDetails);
+            String newAccessToken = tokenDto.getAccessToken();
+            int refreshTokenExpirationMinutes = jwtTokenProvider.getRefreshTokenExpirationMinutes();
+            redisService.setValues(refreshToken, newAccessToken,
+                    Duration.ofMinutes(refreshTokenExpirationMinutes));
+            return newAccessToken;
+        } else throw new BusinessLogicException(ExceptionCode.TOKEN_IS_NOT_SAME);
+    }
+
+    public void logout(String encryptedRefreshToken, String accessToken) {
+        verifiedRefreshToken(encryptedRefreshToken);
+        String refreshToken = aes128Config.decryptAes(encryptedRefreshToken);
+        Claims claims = jwtTokenProvider.parseClaims(refreshToken);
+        String email = claims.getSubject();
+        String redisRefreshToken = redisService.getValues(email);
+        if (!redisService.validateValues(redisRefreshToken)) {
+            redisService.deleteValues(email);
+
+            // 로그아웃 시 Access Token Redis 저장 ( key = Access Token / value = "logout" )
+            int accessTokenExpirationMinutes = jwtTokenProvider.getAccessTokenExpirationMinutes();
+            redisService.setValues(accessToken, "logout", Duration.ofMinutes(accessTokenExpirationMinutes));
+        }
+    }
+
+    private void verifiedRefreshToken(String encryptedRefreshToken) {
+        if (encryptedRefreshToken == null) {
+            throw new BusinessLogicException(ExceptionCode.HEADER_REFRESH_TOKEN_NOT_EXISTS);
+        }
+    }
+
+    private Member findVerifiedMember(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+    }
+}
