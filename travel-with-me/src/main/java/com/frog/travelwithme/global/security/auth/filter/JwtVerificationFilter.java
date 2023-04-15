ed939cb59754820fe5e0529d8933094281d5de33
jambin <gksmfcksqls@gmail.com>
2023-04-15 19:28:37 +0900

    security: exclude url 수정

--- travel-with-me/src/main/java/com/frog/travelwithme/global/security/auth/filter/JwtVerificationFilter.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/security/auth/filter/JwtVerificationFilter.java
@@ -33,8 +33,8 @@ public class JwtVerificationFilter extends OncePerRequestFilter {
             List.of("/",
                     "/h2",
                     "/members/signup",
-                    "/members/login",
-                    "/members/reissue");
+                    "/auth/login",
+                    "/auth/reissue");
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisService redisService;
 
