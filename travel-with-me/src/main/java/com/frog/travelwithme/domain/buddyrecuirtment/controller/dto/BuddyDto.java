4a5c241ce0049542ae56a066e7e5aee85873ee2e
jambin <gksmfcksqls@gmail.com>
2023-04-17 13:05:22 +0900

    refactor: final, @JsonCreator 애너테이션 제거

--- travel-with-me/src/main/java/com/frog/travelwithme/domain/buddyrecuirtment/controller/dto/BuddyDto.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/domain/buddyrecuirtment/controller/dto/BuddyDto.java
@@ -1,7 +1,5 @@
 package com.frog.travelwithme.domain.buddyrecuirtment.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -28,37 +26,17 @@ public class BuddyDto {
 
     @Getter
     @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class ResponseRecruitment {
-
-        private final String title;
-        private final String content;
-        private final String travelNationality;
-        private final LocalDate travelStartDate;
-        private final LocalDate travelEndDate;
-        private final Long viewCount;
-        private final Long commentCount;
-        private final String nickname;
-        private final String memberImage;
-
-        @JsonCreator
-        private ResponseRecruitment(@JsonProperty("title") String title,
-                                    @JsonProperty("content") String content,
-                                    @JsonProperty("travelNationality") String travelNationality,
-                                    @JsonProperty("travelStartDate") LocalDate travelStartDate,
-                                    @JsonProperty("travelEndDate") LocalDate travelEndDate,
-                                    @JsonProperty("viewCount") Long viewCount,
-                                    @JsonProperty("commentCount") Long commentCount,
-                                    @JsonProperty("nickname") String nickname,
-                                    @JsonProperty("memberImage") String memberImage){
-            this.title = title;
-            this.content = content;
-            this.travelNationality = travelNationality;
-            this.travelStartDate = travelStartDate;
-            this.travelEndDate = travelEndDate;
-            this.viewCount = viewCount;
-            this.commentCount = commentCount;
-            this.nickname = nickname;
-            this.memberImage = memberImage;
-        }
+        private String title;
+        private String content;
+        private String travelNationality;
+        private LocalDate travelStartDate;
+        private LocalDate travelEndDate;
+        private Long viewCount;
+        private Long commentCount;
+        private String nickname;
+        private String memberImage;
     }
 }
