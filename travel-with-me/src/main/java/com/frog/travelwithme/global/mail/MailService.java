1d206642bfe762fccf896ef177e43f5c432d1f2d
jambin <gksmfcksqls@gmail.com>
2023-04-22 12:59:15 +0900

    refactor: mail, member 역할에 맞게 로직 분리

---
+++ travel-with-me/src/main/java/com/frog/travelwithme/global/mail/MailService.java
@@ -0,0 +1,49 @@
+package com.frog.travelwithme.global.mail;
+
+import com.frog.travelwithme.global.exception.BusinessLogicException;
+import com.frog.travelwithme.global.exception.ExceptionCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 작성자: 김찬빈
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/04/20
+ **/
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender emailSender;
+
+    public void sendEmail(String toEmail,
+                          String title,
+                          String text) {
+        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+        try {
+            emailSender.send(emailForm);
+        } catch (RuntimeException e) {
+            log.debug("MailService.sendEmail exception occur toEmail: {}, " +
+                    "title: {}, text: {}", toEmail, title, text);
+            throw new BusinessLogicException(ExceptionCode.UNABLE_TO_SEND_EMAIL);
+        }
+    }
+
+    // 발신할 이메일 데이터 세팅
+    private SimpleMailMessage createEmailForm(String toEmail,
+                                             String title,
+                                             String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(title);
+        message.setText(text);
+
+        return message;
+    }
+}
