61f0bbb5bb05702c3153d4b802d7eced0c77dd0f
Lee JaeHyeok <dhfif718@gmail.com>
2023-05-11 04:04:38 +0900

    chore - #65: dev 환경 설정 후 오류 수정
    1. test 코드 프로파일링 안되어 git actions build시 오류 발생 수정

--- travel-with-me/src/test/java/com/frog/travelwithme/unit/redis/service/RedisCrudTest.java
+++ travel-with-me/src/test/java/com/frog/travelwithme/unit/redis/service/RedisCrudTest.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
+@ActiveProfiles("test")
 @Slf4j
 @SpringBootTest
 class RedisCrudTest {
