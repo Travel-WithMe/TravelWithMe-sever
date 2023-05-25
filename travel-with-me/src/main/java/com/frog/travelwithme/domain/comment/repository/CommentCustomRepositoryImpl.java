f094ac945ef20efde777e8a0aaea61ddd68bc204
Lee JaeHyeok <dhfif718@gmail.com>
2023-05-25 18:14:23 +0900

    feat - #90: 댓글,대댓글 초기설계
    1. Entity, Controller, Service, Repository, Mapper 패키지 및 클래스,인터페이스 생성
    2. Comment 도메인 Entity 필드지정

---
+++ travel-with-me/src/main/java/com/frog/travelwithme/domain/comment/repository/CommentCustomRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.frog.travelwithme.domain.comment.repository;
+
+/**
+ * 작성자: 이재혁
+ * 버전 정보: 1.0.0
+ * 작성일자: 2023/05/24
+ **/
+
+public class CommentCustomRepositoryImpl implements CommentCustomRepository{
+}
