6a6b671b122b69478afde07ea9fa68d102eeeb1e
jambin <gksmfcksqls@gmail.com>
2023-06-02 12:11:42 +0900

    refactor: id 직접할당하도록 수정

--- travel-with-me/src/main/java/com/frog/travelwithme/domain/member/entity/Interest.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/domain/member/entity/Interest.java
@@ -18,7 +18,6 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Interest {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String type;
