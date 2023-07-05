2f618e75b8034beb9d28efe1b9b58b5ee0acee47
jambin <gksmfcksqls@gmail.com>
2023-07-06 01:11:33 +0900

    fix - #148: feed comment 로직 리팩토링 (FeedCommentMapper, FeedComment)

--- travel-with-me/src/main/java/com/frog/travelwithme/domain/feed/mapper/FeedCommentMapper.java
+++ travel-with-me/src/main/java/com/frog/travelwithme/domain/feed/mapper/FeedCommentMapper.java
@@ -5,12 +5,17 @@ import com.frog.travelwithme.domain.feed.entity.Feed;
 import com.frog.travelwithme.domain.feed.entity.FeedComment;
 import com.frog.travelwithme.domain.member.entity.Member;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FeedCommentMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "member", source = "member")
+    @Mapping(target = "feed", source = "feed")
     FeedComment toEntity(CommentDto.Post postDto, Member member, Feed feed);
 
+    @Mapping(target = "commentId", source = "id")
     CommentDto.PostResponse toPostResponseDto(FeedComment feedComment);
 }
